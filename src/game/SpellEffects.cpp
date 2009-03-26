63f4b13cfb8fd4e6ccf6e9d4133af083da5dd918
vincex <vincenzo.spatola@gmail.com>
2009-03-26 16:34:25 +0100

    fix al threat del taunt

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -4129,7 +4129,7 @@ void Spell::EffectTaunt(uint32 /*i*/)
 
     // Also use this effect to set the taunter's threat to the taunted creature's highest value
     if(unitTarget->CanHaveThreatList() && unitTarget->getThreatManager().getCurrentVictim())
-        unitTarget->getThreatManager().addThreat(m_caster,unitTarget->getThreatManager().getCurrentVictim()->getThreat());
+        unitTarget->getThreatManager().addThreat(m_caster,unitTarget->getThreatManager().getCurrentVictim()->getThreat() - unitTarget->getThreatManager().getThreat(m_caster) );
 }
 
 void Spell::EffectWeaponDmg(uint32 i)
