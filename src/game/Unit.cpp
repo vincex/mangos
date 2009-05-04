61e80ae0bd0ae99ae035503237da5ac259c87f72
vincex <vincenzo.spatola@gmail.com>
2009-05-05 01:20:01 +0200

    Trinket lighting capicitator

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7540,13 +7540,9 @@ bool Unit::HandleProcTriggerSpell(Unit *pVictim, uint32 damage, Aura* triggeredB
              // stacking
              CastSpell(this, 37658, true, NULL, triggeredByAura);
              // counting
-             uint32 count = 0;
-             AuraList const& dummyAura = GetAurasByType(SPELL_AURA_DUMMY);
-             for(AuraList::const_iterator itr = dummyAura.begin(); itr != dummyAura.end(); ++itr)
-                 if((*itr)->GetId()==37658)
-                     ++count;
-             // release at 3 aura in stack (cont contain in basepoint of trigger aura)
-             if(count < triggerAmount)
+             Aura * dummy = GetDummyAura(37658);
+             // release at 3 aura in stack
+             if(!dummy || dummy->m_stackAmount <=2)
                  return false;
 
              RemoveAurasDueToSpell(37658);
