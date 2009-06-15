d6a4e2e1c570134d6e3e3f6255de436e182e000d
illusion <illusion@illusion-linux.(none)>
2009-06-15 20:32:24 +0200

    Stack di vampiric da piu preti

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4199,7 +4199,8 @@ bool Unit::AddAura(Aura *Aur)
                         case SPELL_AURA_PERIODIC_MANA_LEECH:
                         case SPELL_AURA_PERIODIC_ENERGIZE:
                         case SPELL_AURA_OBS_MOD_MANA:
-                        case SPELL_AURA_POWER_BURN_MANA:
+                        case SPELL_AURA_POWER_BURN_MANA: break;
+                        case SPELL_AURA_DUMMY: if ( aurSpellInfo->Id == 15286 || aurSpellInfo->SpellIconID == 2213 );
                             break;
                         default:                            // not allow
                             // can be only single (this check done at _each_ aura add
