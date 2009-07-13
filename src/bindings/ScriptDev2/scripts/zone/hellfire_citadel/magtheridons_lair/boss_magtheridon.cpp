7a083efdcb41f50b2e2c754153bc6eecc5324444
illusion <illusion@illusion-linux.(none)>
2009-07-13 18:02:17 +0200

    typo mana drain curator, tongue,mind-numbling immunity per shade of aran, magthridon

--- src/bindings/ScriptDev2/scripts/zone/hellfire_citadel/magtheridons_lair/boss_magtheridon.cpp
+++ src/bindings/ScriptDev2/scripts/zone/hellfire_citadel/magtheridons_lair/boss_magtheridon.cpp
@@ -245,6 +245,7 @@ struct MANGOS_DLL_DECL boss_magtheridonAI : public ScriptedAI
         m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
         m_creature->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_UNK_8);
         m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+        m_creature->ApplySpellImmune(0, IMMUNITY_STATE,SPELL_AURA_HASTE_SPELLS, true);
     }
 
     void SetClicker(uint64 cubeGUID, uint64 clickerGUID)
