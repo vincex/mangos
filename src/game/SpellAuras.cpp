5001dfe198e9ea022d7e305e30be1a1549406fcc
Vincenzo Spatola <vincex@MacBook-di-Vincenzo-Spatola.local>
2009-09-17 00:57:59 +0200

    NoStackEnrageDeathWish

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -889,6 +889,26 @@ void Aura::_AddAura()
 
     Unit* caster = GetCaster();
 
+	if(caster)
+    {
+        if(GetId() == 12292)
+        {
+            //Rimuovo le aure di enrage(i vari rank) appena viene castato death wish
+            caster->RemoveAurasDueToSpell(14204);
+            caster->RemoveAurasDueToSpell(14203);
+            caster->RemoveAurasDueToSpell(14202);
+            caster->RemoveAurasDueToSpell(14201);
+            caster->RemoveAurasDueToSpell(12880);
+        }
+
+        if(GetSpellProto()->SpellIconID == 95 && GetSpellProto()->SpellVisual==2817)
+        {
+            //Cast dei vari rank di enrage. se il caster è sotto death wish non applichiamo l'effetto.
+            if(caster->HasAura(12292, 0))
+                return;
+        }
+    }
+
     // set infinity cooldown state for spells
     if(caster && caster->GetTypeId() == TYPEID_PLAYER)
     {
