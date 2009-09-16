78cf2fac3579852ff75bc62902a9405ec422801b
Vincenzo Spatola <vincex@MacBook-di-Vincenzo-Spatola.local>
2009-09-17 00:41:41 +0200

    No Walk su Disorienting

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -350,7 +350,7 @@ void Unit::SendMonsterMove(float NewPosX, float NewPosY, float NewPosZ, uint8 ty
     }
 
     //Movement Flags (0x0 = walk, 0x100 = run, 0x200 = fly/swim)
-    data << uint32(flags);
+    data << uint32(GetTypeId() == TYPEID_PLAYER ? MOVEMENTFLAG_WALK_MODE : MovementFlags);
 
     if(flags & MONSTER_MOVE_WALK)
         Time *= 1.05f;
