9ad61393bcaba20cb2327c7c1d3e962d91646eba
Vincenzo Spatola <vincex@MacBook-di-Vincenzo-Spatola.local>
2009-09-17 00:56:09 +0200

    Resist Stun Melee

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -2307,6 +2307,24 @@ SpellMissInfo Unit::MeleeSpellHitResult(Unit *pVictim, SpellEntry const *spell)
     if (roll < tmp)
         return SPELL_MISS_MISS;
 
+	int32 resist_mech = 0;
+    // Get effects mechanic and chance
+    for(int eff = 0; eff < 3; ++eff)
+    {
+        int32 effect_mech = GetEffectMechanic(spell, eff);
+        if (effect_mech)
+        {
+            int32 temp = pVictim->GetTotalAuraModifierByMiscValue(SPELL_AURA_MOD_MECHANIC_RESISTANCE, effect_mech);
+            if (resist_mech < temp)
+                resist_mech = temp;
+        }
+    }
+    // Apply mod
+    int32 ResistChance = resist_mech*100;
+    uint32 rand = urand(0,10000);
+    if (rand < ResistChance)
+        return SPELL_MISS_RESIST;
+
     // Same spells cannot be parry/dodge
     if (spell->Attributes & SPELL_ATTR_IMPOSSIBLE_DODGE_PARRY_BLOCK)
         return SPELL_MISS_NONE;
